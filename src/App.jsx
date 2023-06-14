6267501ff8bd5b30e0f659428aaa5ba2b27eec5a
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-06-13 22:55:57 -0300

    Navbar in process

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavbarContainer from './components/layouts/navbar/NavbarContainer'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<NavbarContainer/>}/>
+    </Routes>
+    </BrowserRouter>
 
     </>
   )
---
+++ src/components/layouts/navbar/NavbarContainer.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import NavbarPresentacional from "./NavbarPresentacional"
+
+
+const NavbarContainer = () => {
+    const pages = ['Paquetes', 'Vuelos', 'Hoteles', 'Alquileres', 'Blogs'];
+    const settings = ['Perfil', 'Alertas', 'Historial', 'Logout'];
+
+    const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  
+    return (
+    <div>
+        <NavbarPresentacional handleCloseNavMenu = {handleCloseNavMenu}  handleOpenUserMenu={handleOpenUserMenu} handleCloseUserMenu={handleCloseUserMenu} handleOpenNavMenu={handleOpenNavMenu} pages={pages} settings={settings} anchorElNav={anchorElNav} setAnchorElNav={setAnchorElNav} anchorElUser={anchorElUser} setAnchorElUser={setAnchorElUser}/>
+    </div>
+
+
+  )
+  
+}
+
+export default NavbarContainer;
