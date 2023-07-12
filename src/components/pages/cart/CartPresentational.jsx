37547ff955517f4ebfe8bb1c2ef0a5c3273f91ce
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-07-11 21:59:00 -0300

    cartView Done

--- src/components/pages/cart/CartPresentational.jsx
+++ src/components/pages/cart/CartPresentational.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 
 const CartPresentational = ({realizarCompra, cart, removeById, cleanCart}) => {
@@ -8,23 +9,25 @@ const CartPresentational = ({realizarCompra, cart, removeById, cleanCart}) => {
         <h1>Carrito</h1>
   
         <div>
-          {
-            cart.map((product)=>{
+          {cart.length === 0 ?('No hay items', (<Link to={'/'}><button>Volver a la página principal</button></Link>)) : 
+            (cart.map((product)=>{
               return (
                 <div key = {product.id}>
                   <h2>{product.title}</h2>
                   <h3>{product.price}._</h3>
                   <button onClick={()=>removeById(product.id)}>Borrar</button>
                 </div>
-                
               )
-            })
+            }), ( <button onClick={cleanCart}>Vaciar carrito</button>))
+            
+            
           }
-
+          
+          <button onClick={cleanCart}>Vaciar carrito</button>
+            <button onClick={realizarCompra}>Comprar</button>
+      
         </div>
-        <button onClick={cleanCart}>Vaciar carrito</button>
-        <button onClick={realizarCompra}>Comprar</button>
-    
+
     </div>
   )
 }
